a6c32202491d3862b4c51c8e680dedc80b0802a4
raulodev <raulodev@gmail.com>
2025-08-20 08:34:37 -0400

    add context manager to clom handler

--- app/bot/clon/handlers.py
+++ app/bot/clon/handlers.py
@@ -2,8 +2,10 @@ from telegram.ext import CommandHandler, ContextTypes
 
 from app.i18n import _
 from app.utils.custom_update import Update
+from app.utils.decorators import contextmanager
 
 
+@contextmanager
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_html(text=_("Hi from bot clon"))
