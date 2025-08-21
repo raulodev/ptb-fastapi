a5d58e0e7a76ff638c6ec28ad80fc7676a58077e
raulodev <raulodev@gmail.com>
2025-08-21 11:48:00 -0400

    change custom Update

--- app/utils/custom_update.py
+++ app/utils/custom_update.py
@@ -6,10 +6,13 @@ from app.database.models import Bot, User, UserContext
 
 class Update(TgUpdate):
 
-    db_bot = Bot
-    db_user = User
-    db_user_context = UserContext
-    db_session: Session
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.db_bot: Bot | None = None
+        self.db_user: User | None = None
+        self.db_user_context: UserContext | None = None
+        self.db_session: Session | None = None
 
     def __setattr__(self, key, value):
         object.__setattr__(self, key, value)
