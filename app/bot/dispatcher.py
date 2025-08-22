e2ac6003bcca22523765e82c4f3312dc48576382
raulodev <raulodev@gmail.com>
2025-08-22 11:45:11 -0400

    update process_telegram_event

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -13,32 +13,38 @@ async def process_telegram_event(update_json: dict, session: SessionDep, bot: Bo
 
     update = Update.de_json(update_json, application.bot)
 
+    user_id = None
+    first_name = None
+    username = None
+
     if update.my_chat_member:
         user_id = update.my_chat_member.from_user.id
         first_name = update.my_chat_member.from_user.first_name
         username = update.my_chat_member.from_user.username
 
-    else:
+    elif update.effective_user:
         user_id = update.effective_user.id
         first_name = update.effective_user.first_name
         username = update.effective_user.username
 
-    user = session.get(User, user_id)
-    if not user:
-        user = User(id=user_id, name=first_name, username=username)
-        session.add(user)
-        session.commit()
+    if user_id:
 
-    ctxt = session.get(UserContext, user_id)
-    if not ctxt:
-        ctxt = UserContext(id=user_id, user_id=user_id, bot_id=bot.id)
-        session.add(ctxt)
-        session.commit()
+        user = session.get(User, user_id)
+        if not user:
+            user = User(id=user_id, name=first_name, username=username)
+            session.add(user)
+            session.commit()
 
-    update.db_session = session
-    update.db_bot = bot
-    update.db_user = user
-    update.db_user_context = ctxt
+        ctxt = session.get(UserContext, user_id)
+        if not ctxt:
+            ctxt = UserContext(id=user_id, user_id=user_id, bot_id=bot.id)
+            session.add(ctxt)
+            session.commit()
+
+        update.db_session = session
+        update.db_bot = bot
+        update.db_user = user
+        update.db_user_context = ctxt
 
     if bot.is_father:
         application.add_handlers(father_handlers())
